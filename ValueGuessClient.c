43449769956405662f49b48c01994a682641e3a1
Ben Coomes <bcoomes@g.clemson.edu>
2016-01-29 14:30:32 -0500

    third commit

--- ValueGuessClient.c
+++ ValueGuessClient.c
@@ -5,113 +5,171 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <signal.h>
+#include <time.h>
+#include <setjmp.h>
 #include "Practical.h"
 
-//used as a buffer for whatever recievefrom() returns, 
-//which should only be a single char, but hey, who knows?
-#define MAXSTRINGLENGTH 100
-
-int getNextValue(int lastVal, char statusCode);
+int getNextValue(int lastVal, int statusCode);
 
 char checkValue(int val, int sock, struct addrinfo *servAddr);
 
+double getTime();
+
+void interruptSignalHandler(int signal);
+struct sigaction handler;
+
+
 int main(int argc, char *argv[]){
 
-    if (argc != 3){
-        DieWithUserMessage("Parameter(s)", 
-            "<Server Address/Name> [<Server Port/ Service>]");
+    memset(&handler,0,sizeof(handler));
+    handler.sa_handler = interruptSignalHandler;
+    if(sigaction(SIGINT, &handler, 0) < 0){
+        fprintf(stderr, "ERROR: sigaction() failed");
+        exit(1);
+    }
+    if(sigaction(SIGALRM, &handler, 0) < 0){
+        fprintf(stderr, "ERROR: sigaction() failed");
+        exit(1);
     }
 
+    // parse command line arguments
+    if (argc != 4){
+        DieWithUserMessage("Parameter(s)", 
+            "<Server Address> <Server Port 1> <Server Port 2>");
+    }
     char *server = argv[1];
-    char *servPort = argv[2];
+    char *servPort1 = argv[2];
+    char *servPort2 = argv[3];
 
+    // generate addrinfo linked list using the first port option
     struct addrinfo addrCriteria;
     memset(&addrCriteria, 0, sizeof(addrCriteria));
     addrCriteria.ai_family = AF_UNSPEC;
     addrCriteria.ai_socktype = SOCK_DGRAM;
     addrCriteria.ai_protocol = IPPROTO_UDP;
-
     struct addrinfo *servAddr;
-    int rtnval = getaddrinfo(server, servPort, &addrCriteria, &servAddr);
+    int rtnval = getaddrinfo(server, servPort1, &addrCriteria, &servAddr);
     if (rtnval != 0){
         DieWithUserMessage("getaddrinfo() failed", gai_strerror(rtnval));
     }
 
-    int sock = socket(servAddr -> ai_family, servAddr -> ai_socktype, 
-        servAddr -> ai_protocol);
+    // try to build a socket with port 1 and then port 2
+
+    /*my notes: it seems that sockets can still be built even if the port is closed
+                of course, nothing happens when an incorrect port is used.
+                How can a closed port be detected? should I implement a handshake
+                routine and use a timeout to indicate that the client/server connection
+                is not working?
+    */
+    int sock = socket(servAddr-> ai_family, servAddr-> ai_socktype, 
+        servAddr-> ai_protocol);
     if (sock < 0) {
-        DieWithSystemMessage("socket() failed");
+        puts("Port 1 failed, trying to build socket with port 2...\n");
+
+        freeaddrinfo(servAddr); // Is servAddr usable in getaddrinfo after this?
+        int rtnval = getaddrinfo(server, servPort2, &addrCriteria, &servAddr);
+        if (rtnval != 0){
+            DieWithUserMessage("getaddrinfo() failed", gai_strerror(rtnval));
+        }
+
+        sock = socket(servAddr-> ai_family, servAddr-> ai_socktype, 
+            servAddr-> ai_protocol);
+        if(sock < 0){
+            DieWithUserMessage("Socket()", "failed to create socket, tried both ports");
+        }
     }
 
-    char returnedCode = NULL;
+
+    // Main loop: loop until a value is guessed correctly
+    // 4 is arbitrary, only 0, 1, and 2 are meaningful returnedCode values
+    int returnedCode = 4;
     int nextValue = 0;
-    //THIS IS THE LOOP FORMAT SPECIFIED IN THE SPECS AND IT MUST BE USED
+    int guessCount = 0;
+    double runtime = getTime();
     do {
-        nextValue = getNextValue(nextValue, returnedCode);
-
-        returnedCode = checkValue(nextValue, sock, servAddr);
-
-        // if returnCode == 0, break and print completion message
-        // else, keep looping
+        // start work here, get rid of switch, implement same logic with something else
+        printf("at top of main loop, ");
+        switch(jumpCode){
+            case SIGALRM:
+                // case for when alarm goes off!
+                printf("alarm went off!\n");
+                break;
+            case SIGINT: 
+                //case for when user interrupt is called!
+                goto clean_up;
+                break; // unneccesay, but makes me feel better
+            case 0:
+                //case for when normal execution should occur
+                printf("in case 0 of main loop\n");
+                alarm(1);
+                nextValue = getNextValue(nextValue, returnedCode);
+                returnedCode = checkValue(nextValue, sock, servAddr);
+                guessCount++;
+                alarm(0);
+                break;
+            default:
+                //long jump called, fatal error!
+                exit(1);
+        }
+       
     } while (returnedCode != 0);
 
-    //print completion message here
 
-    //CODE BELOW THIS POINT TO BE REORGANIZED TO FIT INSIDE OF THE ABOVE LOOP
 
-    //Must have guess and guesslen initialized by this point
-    // currently, they ARE NOWHERE TO BE FOUND!!!!
+    clean_up: ;
 
-   
+    double totalTime = getTime() - runtime;
+    //print completion message: 
+    //number of guesses, running time, correctly guessed value
+    printf("%d\t%.2f\t%d\n", guessCount, totalTime, nextValue);
     freeaddrinfo(servAddr);
     close(sock);
     exit(0);
 }
 
-// implement a get next val to run in 0(log(n)) time, where n is the range of int values?
-int getNextValue(int lastVal, char statusCode){
+
+
+int getNextValue(int lastVal, int statusCode){
     int nextVal;
 
-    if (&statusCode == NULL){
-        nextVal = 0;
-    }
-    else if (statusCode == '1'){
-        nextVal = lastVal + 1;
-    }
-    else if (statusCode == '2'){
+    // last guess was too high
+    if (statusCode == 1){
         nextVal = lastVal - 1;
     }
-    else{
-        nextVal = 0;
+    // last guess was too small
+    else if (statusCode == 2){
+        nextVal = lastVal + 31622;
     }
-
+    else{
+        nextVal = lastVal;
+    }
+    
     return nextVal;
 }
 
-char checkValue(int val, int sock, struct addrinfo *servAddr){
-    int *guess = &val;
-    size_t guessLen = sizeof(val);
+char checkValue(int guess, int sock, struct addrinfo *servAddr){
 
     // send package with val to server 
-    ssize_t numbytes = sendto(sock, guess, guessLen, 0,
+    ssize_t bytesSent = sendto(sock, &guess, sizeof(guess), 0,
         servAddr -> ai_addr, servAddr -> ai_addrlen);
-    if (numbytes < 0){
+    if (bytesSent < 0){
         DieWithSystemMessage("sendto() failed");
     }
-    else if (numbytes != guessLen){
+    else if (bytesSent != sizeof(int)){
         DieWithUserMessage("sendto() error", "sent unexpected number of bytes");
     }
     // get response from server
     struct sockaddr_storage fromAddr;
     socklen_t fromAddrLen = sizeof(fromAddr);
 
-    char returnedCode;
-    numbytes = recvfrom(sock, &returnedCode, sizeof(returnedCode), 0, 
+    int returnedCode;
+    ssize_t bytesRecieved = recvfrom(sock, &returnedCode, sizeof(returnedCode), 0, 
         (struct sockaddr*) &fromAddr, &fromAddrLen);
-    if (numbytes < 0){
+    if (bytesRecieved < 0){
         DieWithSystemMessage("recvfrom() failed");
     }
-    else if (numbytes != sizeof(char)){
+    else if (bytesRecieved != sizeof(int)){
         DieWithUserMessage("recvfrom() error", "recieved unexpected number of bytes");
     }
     // verify package recived is from server that package was sent to
@@ -125,10 +183,38 @@ char checkValue(int val, int sock, struct addrinfo *servAddr){
     }
 */
     // evaluate validity of response
-    if (returnedCode != '0' || returnedCode != '1' || returnedCode != '2'){
+    if (returnedCode != 0 && returnedCode != 1 && returnedCode != 2){
         DieWithUserMessage("recvfrom() error", "recieved meaningless value.");
     }
 
     //return response
+    printf("returnedCode: %d\n", returnedCode);
     return returnedCode;
+}
+
+double getTime(){
+    struct timeval curTime;
+    gettimeofday(&curTime, NULL);
+    return (((((double) curTime.tv_sec) * 1000000.0) 
+             + (double) curTime.tv_usec) / 1000000.0);
+}
+
+void interruptSignalHandler(int signalID){
+    printf("in interruptSignalHandler\n");
+    if(signalID == SIGINT){
+        // jump with code indicating that a user interupt occured
+        printf("taking long jump to sigint case\n");
+        longjmp(jumpState, signalID);
+        return;
+    }
+    else if(signalID == SIGALRM){
+        // jump with code indicating that an alarm popped
+        printf("taking long jump to sigalrm case\n");
+        alarm(1);
+        longjmp(jumpState, signalID);
+        return;
+    }
+    else{
+        exit(1);
+    }
 }
--- ValueGuessServer.c
+++ ValueGuessServer.c
@@ -8,11 +8,22 @@
 #include "Practical.h"
 
 #define MAXSTRINGLEN 1000
+#define MAXVAL 1000000000 
+
+struct clientNode {
+    struct sockaddr_storage *client_sockaddr;
+    struct clientNode *next;
+};
+
+int processGuess(int guess, int actual);
+
+int getNewValue();
 
 int main(int argc, char *argv[]){
 
-    if(argc != 2){
-        DieWithUserMessage("Parameter(s)", "<Server Port/ Service>");
+    if(argc < 2 || argc > 3){
+        DieWithUserMessage("Parameter(s)", 
+            "<Server Port/ Service> [initial value]");
     }
 
     char *service = argv[1];
@@ -22,6 +33,111 @@ int main(int argc, char *argv[]){
     addrCriteria.ai_flags = AI_PASSIVE;
     addrCriteria.ai_socktype = SOCK_DGRAM;
     addrCriteria.ai_protocol = IPPROTO_UDP;
+
+    struct addrinfo *servAddr;
+    int rtnVal = getaddrinfo(NULL, service, &addrCriteria, &servAddr);
+    if (rtnVal != 0){
+        // handle getaddrinfo failure
+        DieWithUserMessage("getaddrinfo() failed", gai_strerror(rtnVal));
+    }
+
+    int sock = socket(servAddr->ai_family, servAddr->ai_socktype, servAddr->ai_protocol);
+    if (sock < 0){
+        // handle socket creation error
+        DieWithSystemMessage("socket() failed");
+    }
+
+    if(bind(sock, servAddr->ai_addr, servAddr->ai_addrlen) < 0){
+        // handle binding failure
+        DieWithSystemMessage("bind() failed");
+    }
+
+    freeaddrinfo(servAddr);
+
+    //create the node to the client list head
+    struct clientNode *clientListHead;
+
+    // initialize a struct to hold client info
+    struct sockaddr_storage clntAddr;
+    socklen_t clntAddrLen = sizeof(clntAddr);
+
+
+
+    //generate starting value, either user- specified or random
+    int theValue;
+    if(argc == 3){
+        char *valueString = argv[2];
+        theValue = (int)(strtol(valueString, NULL, 10));
+    }
+    else{
+        theValue = getNewValue();
+    }
+
+
+    long messageCount = 0;
+    while (true) {
+        // recieve value (unknown client!)
+        int recievedValue;
+        ssize_t numBytesRecived = recvfrom(sock, &recievedValue, sizeof(recievedValue), 0, 
+            (struct sockaddr*) &clntAddr, &clntAddrLen);
+        //could be point of failure, if server processes a metric shit ton of messages
+        messageCount++;
+
+
+    /*  // check to see if client is in client list
+        //Yes: use that client
+        //No: create new client object to store data about the new client
+        clientNode *nextClient = clientListHead;
+        bool clientIsInList = false;
+        while (nextClient != NULL && !clientIsInList){
+            // see pg 24 to understand this nonsense
+            uint32_t clientAddress = nextClient -> client_sockaddr -> in_addr.s_addr;
+        }
+
+    */
+
+        //check clients value and generate the return code
+        if (numBytesRecived < 0){
+            // perhaps just return incorrect guess code instead of failing??
+            printf("bytes recieved: %d", (int)numBytesRecived);
+            DieWithSystemMessage("recvfrom() failed");           
+        }
+
+        int returnCode = processGuess(recievedValue, theValue);
+        if (returnCode == 0){
+            theValue = getNewValue();
+        }
+  
+        // send return code to client
+        ssize_t numBytesSent = sendto(sock, &returnCode, sizeof(returnCode), 0,
+            (struct sockaddr *) &clntAddr, sizeof(clntAddr)); 
+        if (numBytesSent != sizeof(int)){
+            DieWithUserMessage("sendto()", "sent unexpected number of bytes");
+        }
+    }
+}
+
+
+// key: (0, correct) (1, too high) (2, too low)
+int processGuess(int guess, int actual){
+    int returnCode = 0;
+    int difference = guess - actual;
+        if (difference > 0){
+            returnCode = 1;
+        }
+        else if (difference < 0){
+            returnCode = 2;
+        }
+
+    return returnCode;
+}
+
+int getNewValue(){
+    double randomRatio = ((double)rand()) / RAND_MAX;
+    int newVal = (int)(randomRatio * MAXVAL);
+    return newVal;
+}
+
 
 
     /*
@@ -51,61 +167,3 @@ int main(int argc, char *argv[]){
         the list, so a head pointer must be passsed, and this head pointer is 
         the pointer to servAddr
     */
-    struct addrinfo *servAddr;
-    int rtnVal = getaddrinfo(NULL, service, &addrCriteria, &servAddr);
-    if (rtnVal != 0){
-        // handle getaddrinfo failure
-        DieWithUserMessage("getaddrinfo() failed", gai_strerror(rtnVal));
-    }
-
-    int sock = socket(servAddr->ai_family, servAddr->ai_socktype, servAddr->ai_protocol);
-    if (sock < 0){
-        // handle socket creation error
-        DieWithSystemMessage("socket() failed");
-    }
-
-    if(bind(sock, servAddr->ai_addr, servAddr->ai_addrlen) < 0){
-        // handle binding failure
-        DieWithSystemMessage("bind() failed");
-    }
-
-    /* 
-    void freeaddrinfo(struct addrinfo *addrlist)
-    this is used to free a linked list of addrinfo structs
-
-    Q: why is the pointer to the head of the list not passed? It seems odd that 
-        getaddrinfo uses a pointer to the first node and freeaddrinfo just uses
-        the address of the first node
-    A: ???
-    */
-    freeaddrinfo(servAddr);
-
-    while(true){
-        struct sockaddr_storage clntAddr;
-        socklen_t clntAddrLen = sizeof(clntAddr);
-
-        // create buffer for messages and then block until one arrives
-        char buffer[MAXSTRINGLEN];
-        ssize_t numBytesRecived = recvfrom(sock, buffer, MAXSTRINGLEN, 0, 
-            (struct sockaddr*) &clntAddr, &clntAddrLen);
-        if(numBytesRecived < 0){
-            // handle recvfrom() failure
-            DieWithSystemMessage("recvfrom() failed");
-        }
-
-        fputs("Handling Client ", stdout);
-        PrintSocketAddress((struct sockaddr *) &clntAddr, stdout);
-        fputc('\n', stdout);
-
-        ssize_t numBytesSent = sendto(sock, buffer, numBytesRecived, 0,
-            (struct sockaddr *) &clntAddr, sizeof(clntAddr)); //why not use clntaddrlen as in line 45? try changing and see what happens
-        if (numBytesSent < 0){
-            // handle sendto() failure
-            DieWithSystemMessage("sendto() failed");
-        }
-        else if(numBytesSent != numBytesRecived){
-            //handle unexpected number of bytes sent
-            DieWithUserMessage("sendto()", "sent unexpected number of bytes");
-        }
-    }
-}
